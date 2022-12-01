c07630f5e5f01d86c30d8efa5fabbca61f2588d1
Pavel Huerta <p.jezraelhuert@gmail.com>
2022-12-01 15:35:24 -0600

    funciones con math

---
+++ JS/06.js
@@ -0,0 +1,15 @@
+//math
+let resultado;
+
+resultado = Math.PI
+resultado = Math.round(3.6);
+resultado = Math.ceil(3.2); //siempre redondea hacia arriba ej : 2.1 = 3
+resultado = Math.floor(3.2); //siempre redondea hacia abajo ej : 2.9 = 2
+resultado = Math.sqrt(144); //raiz cuadradas
+resultado = Math.abs(-150); // convierte numeros negativos en positivos
+resultado = Math.min(2, 4, 15, 7); //muestra el numero mas cercano al cero
+resultado = Math.max(5, 79, 4, 65); //muestra el numero mas alto en una secuencia 
+resultado = Math.random(); //dara un numero al azar que no pasa de 1.0
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado);
