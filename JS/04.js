421456532dc07d9980f01104240e72fe9005a74c
Pavel Huerta <p.jezraelhuert@gmail.com>
2022-12-01 13:06:32 -0600

    numeros

--- JS/04.js
+++ JS/04.js
@@ -1,5 +1,17 @@
 //String o cadenas de texto
 
+const tweet = "Aprendiendo JavaScript con el curso de Desarrollo web Completo"
 const producto = "monitor de 20\"";
-
+const email = "correo@correo.com"
+//length es para una extension
+console.log(tweet.length);
 console.log(producto);
+
+//indexOf es para retornar una posicion
+console.log(tweet.indexOf("JavaScript"));
+console.log(producto.indexOf("Tablet"));
+console.log(email.indexOf("@"));
+
+//includes retorna true o false
+console.log(tweet.includes("JavaScript"));
+console.log(producto.includes("Tablet"));
---
+++ JS/05.js
@@ -0,0 +1,12 @@
+//numbers
+
+const number1 = 200;
+const number2 = 5;
+const number3 = 30.5;
+const number4 = 500;
+const number5 = 80;
+
+console.log(number1 - number5);
+console.log(number2 + number3);
+console.log(number4 / number1);
+console.log(number3 * number5);
