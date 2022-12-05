44e540d2a56d98c11a51486682123fbc31eb1928
Pavel Huerta <p.jezraelhuert@gmail.com>
2022-12-05 10:11:53 -0600

    aprendiendo orden de operaciones e incrementos y decrementos

---
+++ JS/07.js
@@ -0,0 +1,30 @@
+//controlar el orden de las operaciones
+let resultado;
+let resultado2;
+let resultado3;
+
+resultado = 20 + 30 * 5;
+resultado2 = (500 + 200) * 1.12;
+resultado3 = (250 + 500) * .5;
+
+console.log(resultado);
+console.log(Math.round( resultado2));
+console.log(resultado3);
+
+//incrementos
+
+let puntaje = 10;
+
+puntaje++;
+puntaje+= 50;
+
+console.log(puntaje);
+
+//decrementos
+
+let puntaje2 = 10;
+
+puntaje2--;
+puntaje2-= 5;
+
+console.log(puntaje2);
