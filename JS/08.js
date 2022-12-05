36bec95c049b1de15d0cdce2f4699aec4c5db10d
Pavel Huerta <p.jezraelhuert@gmail.com>
2022-12-05 10:35:56 -0600

    aprendiendo concatenacion y strings literals

---
+++ JS/08.js
@@ -0,0 +1,9 @@
+//concatenación
+const nombre = 'juan';
+const email = 'correo@correo.com';
+
+console.log('Nombre Cliente: ' + nombre + ' ' + email);
+
+//strings literals
+
+console.log(`Nombre cliente: ${nombre}  Email: ${email}`)
