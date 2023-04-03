f1d8a1615afd61408ef1eb94103a8cdbf64c1c4a
Pavel Huerta <p.jezraelhuert@gmail.com>
2023-04-02 18:51:35 -0600

    prototype

--- JS/27.js
+++ JS/27.js
@@ -16,12 +16,26 @@ function Producto(nombre, precio, disponible) {
     this.disponible = disponible;
 }
 
+//crear funciones que solo se utilizan en objetos especificos
+
+Producto.prototype.FormatearProducto = function(){
+    return `El producto ${this.nombre} tiene un valor de $ ${this.precio} Totales`;
+}
+
 const Producto2 = new Producto('Monitor Curvo 49"', 800,false);
 const Producto3 = new Producto('PS5', 1800, true);
 const Producto4 = new Producto('Impresora Canon 4021', 900, true);
 const Producto5 = new Producto('Control remoto SAMSUNG', 500, false);
-console.log(Producto2);
-console.log(Producto3);
-console.log(Producto4);
-console.log(Producto5);
 
+
+function FormatearProducto(Producto){
+    return `El producto ${Producto.nombre} tiene un valor de $ ${Producto.precio} Totales`;
+}
+
+
+console.log(Producto2.FormatearProducto());
+console.log(Producto3.FormatearProducto() );
+console.log(Producto4.FormatearProducto() );
+console.log(Producto5.FormatearProducto() );
+
+console.log( FormatearProducto(Producto2) );
