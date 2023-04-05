07b192806af90ccb79b1132c7fd948e1622e842d
Pavel Huerta <p.jezraelhuert@gmail.com>
2023-04-05 15:47:20 -0600

    usando try catch

---
+++ JS/29.js
@@ -0,0 +1,14 @@
+//try catch
+
+const numero1 = 50;
+const numero3 = 90;
+
+
+console.log(numero1);
+try {
+    console.log(numero2);
+} catch (error) {
+    console.log(error);
+}
+
+console.log(numero3);
